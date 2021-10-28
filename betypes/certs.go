75d8ac465b6a0e5eb75396d4b6dea255804705b5
SpaceHead1C <damh17@gmail.com>
2021-10-28 17:16:18 +0300

    refactor: toml configs

--- betypes/certs.go
+++
@@ -1,6 +0,0 @@
-package betypes
-
-const (
-	CERT_PATH = "/etc/ssl/certs/cert.crt"
-	KEY_PATH  = "/etc/ssl/certs/cert.key"
-)
--- betypes/configs.go
+++
@@ -1,9 +0,0 @@
-package betypes
-
-const (
-	BOT_TOKEN    = "############"
-	BOT_ADDRESS  = "###.###.###.###"
-	BOT_PORT     = "#####"
-	TELEGRAM_URL = "https://api.telegram.org/bot"
-	LOGS_PATH    = "/var/log/telegram_bot.log"
-)
