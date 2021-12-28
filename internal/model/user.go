b3fd692ca93eb0db7e3322d50788c2eaa41fd720
SpaceHead1C <damh17@gmail.com>
2021-12-29 00:35:41 +0300

    feat: user's statuses and roles

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,7 +1,21 @@
 package model
 
+const (
+	userStatusUndefind  = iota
+	userStatusVisitor   // not in room
+	userStatusGamer     // in room
+	userStatusInGame    // wait estimation process
+	userStatusEstimator // estimates
+)
+
+const (
+	userRoleUndefind = iota
+	userRoleGamer
+	userRoleScrumMaster
+)
+
 type User struct {
-	ID   uint
-	Name string
+	ID           uint
+	Name         string
 	ActivityMark uint64
 }
